61dcf6b0bd5877ead5693de7ffd970329fbffb77
jdaws97 <jarod.daws@gmail.com>
2022-04-22 13:39:37 -0400

    initial commit

---
+++ standup_alias.sh
@@ -0,0 +1,6 @@
+CONFIG_PATH=$(grep -oP '"path": "\K[^"\047]+(?=["\047])' /tmp/standup-config.json)
+echo $PATH
+
+CURRENT_PATH=$(pwd)
+
+echo "alias standup='python3 ${CURRENT_PATH}/main.py'" >> ~/.bashrc
